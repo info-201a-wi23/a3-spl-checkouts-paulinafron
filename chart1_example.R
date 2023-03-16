f384c298b5519a78cdfd054483ec3da9a11153ad
paulinafron <paulina@froncek.com>
2023-03-16 02:33:17 -0700

    some_changes

--- chart1_example.R
+++ chart1_example.R
@@ -1,6 +1,19 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
+#Set the working directory
+setwd("~/Desktop/INFO201/a3-spl-checkouts-paulinafron")
 
-library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+#Load packages
+library("dplyr")
+library("tidyverse")
+library("ggplot2")
+
+#Load the data
+library_checkouts <- read_csv("Checkouts_by_Title.csv")
+
+values_chart_1 <- library_checkouts %>% 
+  group_by(UsageClass, CheckoutYear) %>%
+  summarise(total = sum(Checkouts))
+
+chart1 <- ggplot(data = values_chart_1) +
+  geom_line(aes(x = CheckoutYear, y = total, color = UsageClass)) +
+  labs(x = "Total checkouts", y = "Year", title = "Total checkouts per year", colour = "Item type") +
+  theme(legend.position = "bottom")
--- chart2_example.R
+++ chart2_example.R
@@ -1,6 +1,25 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
+#Set the working directory
+setwd("~/Desktop/INFO201/a3-spl-checkouts-paulinafron")
 
-library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+#Load packages
+library("dplyr")
+library("tidyverse")
+library("ggplot2")
+
+#Load the data
+library_checkouts <- read_csv("Checkouts_by_Title.csv")
+
+values_chart_2 <- library_checkouts %>% filter((Title == "Educated: A Memoir") | 
+                                         (Title == "Where the Crawdads Sing") | 
+                                         (Title == "Becoming") | 
+  (Title == "White Fragility: Why It's So Hard for White People to Talk About Racism") |
+  (Title == "Me and White Supremacy: Combat Racism, Change the World, and Become a Good Ancestor") |
+  (Title == "How to Be an Antiracist")) %>% 
+  group_by(Title, CheckoutYear) %>%
+  summarise(total = sum(Checkouts))
+
+chart2 <- ggplot(data = values_chart_2) +
+  geom_line(aes(x = CheckoutYear, y = total, color = Title)) +
+  labs(x = "Total checkouts", y = "Year", title = "Total checkouts per year - popular titles", colour = "Title") +
+  theme(legend.position = "bottom") +
+  guides(colour = guide_legend(nrow = 6, byrow = TRUE))
